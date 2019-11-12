e27cbd5c1196240a924979bdf1e578eff2fe6670
xDecus <guitarx3@gmail.com>
2019-11-12 21:16:39 +0100

    Add full PWA support, 100 Lighthouse scores

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,7 @@ const Home: NextPage = () => (
             <meta name="theme-color" content="#72B340" />
             <meta name="description" content="Portfolio Page" />
         </Head>
+        <h1 className="text-purple-700 text-4xl">Welcome!</h1>
     </div>
 );
 
