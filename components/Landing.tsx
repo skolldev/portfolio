df51b30e04271d3a947946df7fd905be30cc86da
xDecus <guitarx3@gmail.com>
2019-11-16 15:50:20 +0100

    remove buggy PWA

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,9 +1,23 @@
-import { faGit, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faGit, faGithub, IconDefinition } from "@fortawesome/free-brands-svg-icons";
 import { faFileAlt } from "@fortawesome/free-regular-svg-icons";
 import { faAt, faFile } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
-import WhoAmI from "./WhoAmI";
+import TypeWriter from "./Typewriter";
+
+interface ILinkProps {
+    link: string;
+    icon: IconDefinition;
+    text: string;
+}
+const IconLink = (props: ILinkProps) => (
+    <a className="hover:text-purple-600 mr-6" href={props.link} target="blank">
+        <span className="inline-block text-2xl text-center mr-2 align-text-bottom">
+            <FontAwesomeIcon icon={props.icon} />
+        </span>
+        <span>{props.text}</span>
+    </a>
+);
 
 export default function Landing() {
     return (
@@ -17,7 +31,12 @@ export default function Landing() {
             <h1 className="text-2xl font-bold mb-2">
                 My name is <span className="text-purple-700">Alexander May</span>
             </h1>
-            <WhoAmI className="" />
+            <h1 className={`text-xl`}>
+                I'm a{" "}
+                <TypeWriter
+                    items={["frontend dev", "powerlifter", "backend dev", "coach", "nerd"]}
+                />
+            </h1>
             <p className="text-lg max-w-xl text-left mb-2 leading-relaxed">
                 currently living in munich. I love building awesome web applications, learning new
                 things, cooking, lifting heavy stuff and teaching other people how to lift heavy
@@ -25,28 +44,9 @@ export default function Landing() {
             </p>
             <p className="text-lg max-w-xl text-center mb-2">I'd love to get to know you!</p>
             <div id="links" className="flex flex-row text-xl">
-                <a className="hover:text-purple-600 mr-6" target="blank">
-                    <span className="inline-block text-2xl text-center w-8 h-8 mr-1 ">
-                        <FontAwesomeIcon icon={faFileAlt} />
-                    </span>
-                    <span>CV</span>
-                </a>
-                <a
-                    className="hover:text-purple-600 mr-6"
-                    href="https://github.com/xDecus"
-                    target="blank"
-                >
-                    <span className="inline-block text-2xl text-center w-8 h-8 mr-1">
-                        <FontAwesomeIcon icon={faGithub} />
-                    </span>
-                    <span>Github</span>
-                </a>
-                <a className="hover:text-purple-600" href="mailto:alex.may.ush@web.de">
-                    <span className="inline-block text-2xl text-center w-8 h-8 mr-1">
-                        <FontAwesomeIcon icon={faAt} />
-                    </span>
-                    <span>Email</span>
-                </a>
+                <IconLink text="CV" link="https://github.com/xDecus" icon={faFileAlt} />
+                <IconLink text="Github" link="https://github.com/xDecus" icon={faGithub} />
+                <IconLink text="Email" link="mailto:alex.may.ush@web.de" icon={faAt} />
             </div>
         </div>
     );
