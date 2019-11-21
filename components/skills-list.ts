48b0a8c4da2df384c183eb704af7ef128dfd81e0
xDecus <guitarx3@gmail.com>
2019-11-21 14:59:17 +0100

    add svg for each skill, extract to list file

---
+++ components/skills-list.ts
@@ -0,0 +1,91 @@
+interface ISkill {
+    name: string;
+    iconUrl?: string;
+}
+
+export const skills: ISkill[] = [
+    {
+        name: "React.js",
+        iconUrl: "/skills/react.svg"
+    },
+    {
+        name: "Angular",
+        iconUrl: "/skills/angular.svg"
+    },
+    {
+        name: "Javascript",
+        iconUrl: "/skills/javascript.svg"
+    },
+    {
+        name: "Typescript",
+        iconUrl: "/skills/typescript.svg"
+    },
+    {
+        name: "HTML/CSS",
+        iconUrl: "/skills/html.svg"
+    },
+    {
+        name: "SCSS",
+        iconUrl: "/skills/sass.svg"
+    },
+    {
+        name: "Jasmine",
+        iconUrl: "/skills/jasmine.svg"
+    },
+    {
+        name: "Jest",
+        iconUrl: "/skills/jest.svg"
+    },
+    {
+        name: "Tailwind CSS",
+        iconUrl: "/skills/tailwind.svg"
+    },
+    {
+        name: "Git",
+        iconUrl: "/skills/git.svg"
+    },
+    {
+        name: "SVN",
+        iconUrl: "/skills/subversion.svg"
+    },
+    {
+        name: "Next.js",
+        iconUrl: "/skills/next-js.svg"
+    },
+    {
+        name: "GitLab",
+        iconUrl: "/skills/gitlab.svg"
+    },
+    {
+        name: "GitHub",
+        iconUrl: "/skills/github.svg"
+    },
+    {
+        name: ".NET/EF Core",
+        iconUrl: "/skills/net-core.svg"
+    },
+    {
+        name: "C#",
+        iconUrl: "/skills/sharp.svg"
+    },
+    {
+        name: "Jenkins",
+        iconUrl: "/skills/jerkins.svg"
+    },
+    {
+        name: "MSSQL",
+        iconUrl: "/skills/mssql.svg"
+    },
+    {
+        name: "Protractor",
+        iconUrl: "/skills/protractor.svg"
+    },
+    {
+        name: "IIS",
+        iconUrl: "/skills/microsoft.svg"
+    },
+    {
+        name: "Advanced Installer",
+        iconUrl: "/skills/ai.svg"
+    }
+];
