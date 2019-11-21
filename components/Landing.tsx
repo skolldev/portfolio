edd4637d7a34155762d8906ddadbb07c8b941937
skolldev <guitarx3@gmail.com>
2019-11-21 19:28:32 +0100

    use webp format

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -24,11 +24,15 @@ export default function Landing() {
     return (
         <div className="px-8 lg:p-0 text-black flex flex-col justify-center items-start block h-screen">
             <div className=" text-black flex flex-col lg:flex-row items-start w-full justify-center">
-                <img
-                    src="./portrait.png"
-                    className="rounded-full shadow-md mb-8 lg:mb-0 h-64 w-64 xl:h-80 xl:w-80 self-center lg:order-1 object-cover"
-                    alt="Picture of me"
-                />
+                <picture className="self-center lg:order-1">
+                    <source srcSet="/portrait.webp" type="image/webp" className="rounded-full" />
+                    <source srcSet="./portrait.jpg" type="image/jpg" className="rounded-full" />
+                    <img
+                        src="./portrait.jpg"
+                        className="rounded-full shadow-md mb-8 lg:mb-0 h-64 w-64 xl:h-80 xl:w-80 object-cover"
+                        alt="Picture of me"
+                    />
+                </picture>
                 <div className="flex flex-col pt-2 xl:pt-0 lg:mr-8 xl:mr-12">
                     <h1 className="text-2xl xl:text-3xl font-bold mb-2">
                         My name is <span className="text-purple-700">Alexander May</span>
